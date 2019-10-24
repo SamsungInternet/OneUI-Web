35e1d6010a653eddf48af420ae3b477612ea6756
Ada Rose Cannon <ada@ada.is>
2019-10-24 17:28:55 +0100

    run tests on all examples

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 const { spawn } = require('child_process');
+const util = require('util');
+const glob = util.promisify(require('glob'));
 
 const docsify = spawn('docsify', ['serve']);
 docsify.stdout.on('data', (data) => {
@@ -12,20 +14,29 @@ docsify.on('close', (code) => {
 let browser;
 
 (async () => {
+  const urls = (await glob('examples/**/*'));
+  urls.push('');
+
   await new Promise(r => setTimeout(r,1000));
+
   browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://127.0.0.1:3000');
-  await await page.addScriptTag({url: '/node_modules/axe-core/axe.min.js'})
-  const results = await page.evaluate(() => {
-    return new Promise(resolve => {
-      axe.run(function (err, results) {
-        if (err) throw err;
-        resolve(results);
+
+  for (const url of urls) {
+    console.log(url);
+    const page = await browser.newPage();
+    await page.goto('http://127.0.0.1:3000/' + url);
+    await page.addScriptTag({url: '/node_modules/axe-core/axe.min.js'})
+    const results = await page.evaluate(() => {
+      return new Promise(resolve => {
+        axe.run(function (err, results) {
+          if (err) throw err;
+          resolve(results);
+        });
       });
     });
-  });
-  console.log(results);
+    await page.close();
+    console.log(results);
+  }
 })()
 .catch(e => console.warn(e))
 .then(async () => {
