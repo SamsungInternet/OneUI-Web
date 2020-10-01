ba2bd18f852a401745e5a0cf95c53e469c060ca7
Ada Rose Cannon <ada@ada.is>
2020-10-01 12:14:07 +0100

    fix linting

--- oui-css/scripts/oui.js
+++ oui-css/scripts/oui.js
@@ -6,44 +6,44 @@ function onkeydown(e) {
 
 	if (!element) return;
 
-    const currentFocusEl = element.querySelector(":focus");
+	const currentFocusEl = element.querySelector(":focus");
 
 	const letterKey = e.code.match(/^Key([A-Z])$/);
-    if (letterKey) {
-        element.open = true;
-        const letter = letterKey[1].toLowerCase();
-        const elsAfter = Array.from(
-            element.querySelectorAll("li:focus-within ~ li")
-        );
-        for (const el of elsAfter) {
-            if (el.textContent.trim().toLowerCase()[0] === letter) {
-                return el.querySelector("a").focus();
-            }
-        }
-        const allEls = Array.from(element.querySelectorAll("li"));
-        for (const el of allEls) {
-            if (el.textContent.trim().toLowerCase()[0] === letter) {
-                return el.querySelector("a").focus();
-            }
-        }
-        return;
-    }
+	if (letterKey) {
+		element.open = true;
+		const letter = letterKey[1].toLowerCase();
+		const elsAfter = Array.from(
+			element.querySelectorAll("li:focus-within ~ li")
+		);
+		for (const el of elsAfter) {
+			if (el.textContent.trim().toLowerCase()[0] === letter) {
+				return el.querySelector("a").focus();
+			}
+		}
+		const allEls = Array.from(element.querySelectorAll("li"));
+		for (const el of allEls) {
+			if (el.textContent.trim().toLowerCase()[0] === letter) {
+				return el.querySelector("a").focus();
+			}
+		}
+		return;
+	}
 
 	if (currentFocusEl.tagName === "SUMMARY") {
 		switch (e.code) {
-			case "ArrowUp":
-				e.preventDefault();
-				element.open = true;
-				element.querySelector("li:last-child>a").focus();
-				return false;
+		case "ArrowUp":
+			e.preventDefault();
+			element.open = true;
+			element.querySelector("li:last-child>a").focus();
+			return false;
 
-			case "ArrowDown":
-			case "Space":
-			case "Return":
-				e.preventDefault();
-				element.open = true;
-				element.querySelector("li:first-child>a").focus();
-				return false;
+		case "ArrowDown":
+		case "Space":
+		case "Return":
+			e.preventDefault();
+			element.open = true;
+			element.querySelector("li:first-child>a").focus();
+			return false;
 		}
 	}
 
@@ -51,62 +51,62 @@ function onkeydown(e) {
 		let el, prevEl;
 
 		switch (e.code) {
-			case "ArrowUp":
-				e.preventDefault();
-				el = element.querySelector("li:focus-within");
-				prevEl =
+		case "ArrowUp":
+			e.preventDefault();
+			el = element.querySelector("li:focus-within");
+			prevEl =
 					el.previousElementSibling ||
 					element.querySelector("li:last-child");
-				prevEl.querySelector("a").focus();
-				return false;
+			prevEl.querySelector("a").focus();
+			return false;
 
-			case "ArrowDown":
-				e.preventDefault();
-				el =
+		case "ArrowDown":
+			e.preventDefault();
+			el =
 					element.querySelector("li:focus-within+li>a") ||
 					element.querySelector("li:first-child>a");
-				el.focus();
-				return false;
+			el.focus();
+			return false;
 
-			case "Enter":
-			case "Escape":
-				element.open = false;
-				element.querySelector("summary").focus();
-				break;
+		case "Enter":
+		case "Escape":
+			element.open = false;
+			element.querySelector("summary").focus();
+			break;
 
-			case "Home":
-				e.preventDefault();
-				element.querySelector("li:first-child>a").focus();
-				break;
+		case "Home":
+			e.preventDefault();
+			element.querySelector("li:first-child>a").focus();
+			break;
 
-			case "End":
-				e.preventDefault();
-				element.querySelector("li:last-child>a").focus();
-				break;
+		case "End":
+			e.preventDefault();
+			element.querySelector("li:last-child>a").focus();
+			break;
 		}
 	}
 }
 
 function onblur(e) {
-    const element = e.currentTarget;
-    if (element.matches(':focus-within')) return;
-    element.open = false;
+	const element = e.currentTarget;
+	if (element.matches(':focus-within')) return;
+	element.open = false;
 }
 
 function ontoggle(e) {
-    const element = e.currentTarget;
-    if (element.querySelector('ul').matches(':focus-within')) return;
-    if (element.open) element.querySelector("li:first-child>a").focus();
+	const element = e.currentTarget;
+	if (element.querySelector('ul').matches(':focus-within')) return;
+	if (element.open) element.querySelector("li:first-child>a").focus();
 }
 
 function ouiBindKeys() {
-    for (const el of [...document.querySelectorAll(".oui-menu")]) {
-        el.removeEventListener("blur", onblur);
-        el.removeEventListener("toggle", ontoggle);
-        el.removeEventListener("keydown", onkeydown);
-        el.addEventListener("blur", onblur, true);
-        el.addEventListener("toggle", ontoggle);
-        el.addEventListener("keydown", onkeydown);
-    }
+	for (const el of [...document.querySelectorAll(".oui-menu")]) {
+		el.removeEventListener("blur", onblur);
+		el.removeEventListener("toggle", ontoggle);
+		el.removeEventListener("keydown", onkeydown);
+		el.addEventListener("blur", onblur, true);
+		el.addEventListener("toggle", ontoggle);
+		el.addEventListener("keydown", onkeydown);
+	}
 }
 ouiBindKeys();
